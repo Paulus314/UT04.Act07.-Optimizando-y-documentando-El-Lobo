8bbf05820bfb12ab594c1b383880adf96b737136
Palus314 <pablohleon5@gmail.com>
2026-04-20 11:51:53 +0100

    Primer commit de refactorización

---
+++ main.py
@@ -0,0 +1,18 @@
+from partes import gestorPartida
+
+# --- Ejecución caótica ---
+juego=gestorPartida()
+juego.anadirJugador("Nacho", "lobo")
+juego.anadirJugador("Elena", "vidente")
+juego.anadirJugador("Carlos","aldeano")
+
+print(juego.jugadores[0].AccionNocturna(juego.jugadores[2]))
+print(juego.ComprobarVictoria())
+
+
+# Intenta estructurar el main.py con el siguiente orden.
+# --- FASE 1: LA NOCHE 🌙 ---
+# Comprobamos estado antes de que amanezca
+# --- FASE 2: EL DÍA ☀️ ---
+# El motor de la partida ejecuta el linchamiento
+# --- FINAL DE LA PARTIDA ---
